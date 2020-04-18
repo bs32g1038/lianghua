df53dce217937357d03fb7e7e13d8c19037cb4a9
bs32g1038 <bs32g1038@163.com>
2020-04-18 22:19:44 +0800

    chore: adjust name

--- src/components/chart/index.vue
+++ src/components/chart/index.vue
@@ -55,21 +55,23 @@ export default {
                 data: [
                     '完整-小时K线',
                     '简易-小时k线',
-                    'long止损--开盘',
-                    'long止损--收盘',
-                    'short平仓--开盘',
-                    'short平仓--收盘',
-                    'long止损-k线',
-                    'short平仓-k线',
+                    'Long开仓',
+                    'Long平仓',
+                    'Short开仓',
+                    'Short平仓',
+                    'Long-k线',
+                    'Short-k线',
+                    '止盈平仓-k线',
                 ],
                 selected: {
                     '完整-小时K线': false,
-                    'long止损--开盘': false,
-                    'long止损--收盘': false,
-                    'short平仓--开盘': false,
-                    'short平仓--收盘': false,
-                    'long止损-k线': false,
-                    'short平仓-k线': false,
+                    'Long开仓': false,
+                    'Long平仓': false,
+                    'Short开仓': false,
+                    'Short平仓': false,
+                    'Long-k线': false,
+                    'Short-k线': false,
+                    '止盈平仓-k线': false,
                 },
             },
             tooltip: {
@@ -129,27 +131,6 @@ export default {
                     end: 100,
                 },
             ],
-            // dataZoom: [
-            //     {
-            //         type: 'inside',
-            //         start: 0,
-            //         end: 10,
-            //     },
-            //     {
-            //         start: 0,
-            //         end: 10,
-            //         handleIcon:
-            //             'M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z',
-            //         handleSize: '80%',
-            //         handleStyle: {
-            //             color: '#fff',
-            //             shadowBlur: 3,
-            //             shadowColor: 'rgba(0, 0, 0, 0.6)',
-            //             shadowOffsetX: 2,
-            //             shadowOffsetY: 2,
-            //         },
-            //     },
-            // ],
             series: [
                 {
                     name: '完整-小时K线',
@@ -160,92 +141,7 @@ export default {
                         color0: downColor,
                         borderColor: upBorderColor,
                         borderColor0: downBorderColor,
-                    },
-                    markPoint: {
-                        label: {
-                            normal: {
-                                formatter: function (param) {
-                                    return param != null ? Math.round(param.value) : '';
-                                },
-                            },
-                        },
-                        data: [
-                            {
-                                name: 'XX标点',
-                                coord: ['2013/5/31', 2300],
-                                value: 2300,
-                                itemStyle: {
-                                    color: 'rgb(41,60,85)',
-                                },
-                            },
-                            {
-                                name: 'highest value',
-                                type: 'max',
-                                valueDim: 'highest',
-                            },
-                            {
-                                name: 'lowest value',
-                                type: 'min',
-                                valueDim: 'lowest',
-                            },
-                            {
-                                name: 'average value on close',
-                                type: 'average',
-                                valueDim: 'close',
-                            },
-                        ],
-                        tooltip: {
-                            formatter: function (param) {
-                                return param.name + '<br>' + (param.data.coord || '');
-                            },
-                        },
-                    },
-                    markLine: {
-                        symbol: ['none', 'none'],
-                        data: [
-                            [
-                                {
-                                    name: 'from lowest to highest',
-                                    type: 'min',
-                                    valueDim: 'lowest',
-                                    symbol: 'circle',
-                                    symbolSize: 10,
-                                    label: {
-                                        show: false,
-                                    },
-                                    emphasis: {
-                                        label: {
-                                            show: false,
-                                        },
-                                    },
-                                },
-                                {
-                                    type: 'max',
-                                    valueDim: 'highest',
-                                    symbol: 'circle',
-                                    symbolSize: 10,
-                                    label: {
-                                        show: false,
-                                    },
-                                    emphasis: {
-                                        label: {
-                                            show: false,
-                                        },
-                                    },
-                                },
-                            ],
-                            {
-                                name: 'min line on close',
-                                type: 'min',
-                                valueDim: 'close',
-                            },
-                            {
-                                name: 'max line on close',
-                                type: 'max',
-                                valueDim: 'close',
-                            },
-                        ],
-                    },
+                    }
                 },
                 {
                     name: '简易-小时k线',
@@ -261,7 +157,7 @@ export default {
                     showSymbol: false,
                 },
                 {
-                    name: 'long止损--开盘',
+                    name: 'Long开仓',
                     emphasis: {
                         label: {
                             show: true,
@@ -280,7 +176,7 @@ export default {
                     type: 'scatter',
                 },
                 {
-                    name: 'long止损--收盘',
+                    name: 'Long平仓',
                     emphasis: {
                         label: {
                             show: true,
@@ -299,7 +195,7 @@ export default {
                     type: 'scatter',
                 },
                 {
-                    name: 'short平仓--开盘',
+                    name: 'Short开仓',
                     emphasis: {
                         label: {
                             show: true,
@@ -318,7 +214,7 @@ export default {
                     type: 'scatter',
                 },
                 {
-                    name: 'short平仓--收盘',
+                    name: 'Short平仓',
                     emphasis: {
                         label: {
                             show: true,
@@ -337,7 +233,7 @@ export default {
                     type: 'scatter',
                 },
                 {
-                    name: 'long止损-k线',
+                    name: 'Long-k线',
                     type: 'line',
                     data: longData
                         .map((item) => {
@@ -353,7 +249,7 @@ export default {
                     },
                 },
                 {
-                    name: 'short平仓-k线',
+                    name: 'Short-k线',
                     type: 'line',
                     data: shortData
                         .map((item) => {
@@ -363,7 +259,23 @@ export default {
                             ];
                         })
                         .flat(),
-                    smooth: false,
+                    smooth: true,
+                    lineStyle: {
+                        opacity: 0.5,
+                    },
+                },
+                {
+                    name: '止盈平仓-k线',
+                    type: 'line',
+                    data: tradeData
+                        .map((item) => {
+                            return [
+                                [item.open_date, item.open],
+                                [item.close_date, item.close],
+                            ];
+                        })
+                        .flat(),
+                    smooth: true,
                     lineStyle: {
                         opacity: 0.5,
                     },
